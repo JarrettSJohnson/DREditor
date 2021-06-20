9ffcf7e241143d01ca6144a6808123df68eee814
Jarrett Johnson <jarrettsjohnson@gmail.com>
2021-06-20 16:46:02 -0400

    Dialogue Events

---
+++ Scripts/EventObjects/DialogueEvent.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using DREditor;
+
+[CreateAssetMenu(menuName = "DREditor/Events/Dialogue Event", fileName = "New Dialogue Event")]
+public class DialogueEvent : ScriptableObject
+{
+    private readonly List<DialogueEventListener> listeners = new List<DialogueEventListener>();
+
+    public void Raise() => Enumerable.Reverse(listeners).ForEach(listener => listener.OnEventRaised());
+
+    public void RegisterListener(DialogueEventListener listener) => listeners.Add(listener);
+
+    public void UnregisterListener(DialogueEventListener listener) => listeners.Remove(listener);
+}
